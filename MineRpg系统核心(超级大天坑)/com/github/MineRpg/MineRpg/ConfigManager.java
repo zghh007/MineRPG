9b8d97911e2b5cfa8676bc6b19def6e21bea7e13
Nodak_Lzc <1270410764@qq.com>
2016-03-12 20:53:17 +0800

    修复了各种BUG 护甲空指针未解决

--- MineRpg系统核心(超级大天坑)/com/github/MineRpg/MineRpg/ConfigManager.java
+++ MineRpg系统核心(超级大天坑)/com/github/MineRpg/MineRpg/ConfigManager.java
@@ -1,40 +1,36 @@
 package com.github.MineRpg.MineRpg;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
 
-public class ConfigManager {
-	private MineCore j = JavaPlugin.getPlugin(MineCore.class);
+public class ConfigManager implements Listener {
+	private MineCore j;
+
+	private FileConfiguration File;
 	private File f;
-	private YamlConfiguration y;
 
 	public ConfigManager(String name) {
-		f = new File(j.getDataFolder(), name + ".yml");
-		if (!f.exists()) {
-			try {
-				f.createNewFile();
-
-			} catch (IOException e) {
-				e.printStackTrace();
-
-			}
-		}
-		YamlConfiguration y = YamlConfiguration.loadConfiguration(f);
-
+//		CreateConfig(name);
+		
+	}
+	
+	
+	File CreateConfig(String name){
+		return new File(j.getDataFolder(), name+".yml");
 	}
 
-	@Deprecated
-	public YamlConfiguration getConfig() {
-		return y;
+	public ConfigManager(MineCore mineCore) {
+		this.j = mineCore;
+		return;
 	}
 
 	public boolean save() {
 		try {
-			y.save(f);
+			File.save(f);
 		} catch (Exception e) {
 			return false;
 		}
@@ -42,14 +38,14 @@ public class ConfigManager {
 	}
 
 	public void put(String s, String obj) {
-		y.set(s, obj);
+		File.set(s, obj);
 	}
 
 	public void put(String s, double obj) {
-		y.set(s, obj);
+		File.set(s, obj);
 	}
 
-	public void put(String s, List obj) {
-		y.set(s, obj);
+	public void put(String s, List<?> obj) {
+		File.set(s, obj);
 	}
 }
